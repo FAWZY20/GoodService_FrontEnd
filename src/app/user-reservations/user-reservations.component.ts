3dddb9e2cbf1d23586714f2c1c27839604465239
Fawzy20 <fawzy-elsam@outlook.fr>
2021-06-11 21:41:40 +0200

    ajout des fonctionaliter d'info et de supression

--- src/app/user-reservations/user-reservations.component.ts
+++ src/app/user-reservations/user-reservations.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProfessionalLightDTO } from '../api/professionalLightDTO';
-import { UserLightDTO } from '../api/userLightDTO';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RegistationService } from '../registation.service';
 
 export interface ReservationDTO{
-  id?:number;
+  id:number;
   professional:ProfessionalLightDTO;
-  client:UserLightDTO;
-  appointmentDateTime?:Date;
-  status?:string;
+  prestation: string;
+  address: string;
+  appointementDate:Date;
+  etat:string;
 }
 
 
@@ -17,19 +19,50 @@ export interface ReservationDTO{
   templateUrl: './user-reservations.component.html',
   styleUrls: ['./user-reservations.component.css']
 })
-
 export class UserReservationsComponent implements OnInit {
 
   reservations:ReservationDTO[]=[];
+  private deleteId: number;
 
-  constructor(private http: HttpClient) { }
+  constructor(private _service : RegistationService, private http: HttpClient, private modalService: NgbModal) { }
 
   ngOnInit(): void {
-      this.http.get<ReservationDTO[]>('http://localhost:8080/api/reservation/list').subscribe(dtos=>{
+      this.http.get<ReservationDTO[]>('http://localhost:8080/list').subscribe(dtos=>{
         this.reservations = dtos;
       }, error=>{
         console.error('error occuid', error)
       })
   }
+
+  openDelete(targetModal, reservation: ReservationDTO) {
+    this.deleteId = reservation.id;
+    this.modalService.open(targetModal, {
+      backdrop: 'static',
+      size: 'lg'
+    });
+  }
+
+
+  onDelete() {
+    const deleteURL = 'http://localhost:8080/reservation/' + this.deleteId + '/delete';
+    this.http.delete(deleteURL)
+      .subscribe((results) => {
+        this.ngOnInit();
+        this.modalService.dismissAll();
+      });
+  }
+
+  openDetails(targetModal, reservation: ReservationDTO) {
+    this.modalService.open(targetModal, {
+     centered: true,
+     backdrop: 'static',
+     size: 'lg'
+   });
+   document.getElementById('prestation').setAttribute( 'value' , reservation.prestation);
+   document.getElementById( 'professional' ).setAttribute( 'value' , reservation.professional.nom);
+   document.getElementById( 'date' ).setAttribute( 'value' , reservation.appointementDate.getDay);
+   document.getElementById( 'Adress' ).setAttribute( 'value' , reservation.address);
+   document.getElementById( 'etat' ).setAttribute( 'value' , reservation.etat);
+}
 
 }
